7671a9377ff4a6f027ce85ccf6e2d059c3275579
tarikbir <7569835+tarikbir@users.noreply.github.com>
2017-11-15 10:22:55 +0300

    son hali

--- main.c
+++ main.c
@@ -109,11 +109,7 @@ int main()
     /* INITIALIZATION */
     srand(time(NULL));
     char map[X][X];
-<<<<<<< HEAD
     int deneme123;
-=======
-    int besyuzbin=500;
->>>>>>> e6da164df90f279b0a4efa9ee987e5bc15e2b4af
     /* TODO: SCANF X MALLOC DIZI */
     generateRandomMap(map);
     drawMap(map);
