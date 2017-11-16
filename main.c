8f68b18f3ec477b863cdd749a2109d805c7fe1aa
tarikbir <7569835+tarikbir@users.noreply.github.com>
2017-11-16 16:01:29 +0300

    a yazıldı

--- main.c
+++ main.c
@@ -133,6 +133,7 @@ int main()
     /* INITIALIZATION */
     srand(time(NULL));
     char map[X][X] = {0};
+    int a = 3;
     /* TODO: SCANF X MALLOC DIZI */
     generateRandomMap(map);
     drawMap(map);
